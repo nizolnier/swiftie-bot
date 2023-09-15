e17f963589cca82e232b86bff60cd3c519eb2abe
Nicole Zolnier <nzolnier@gmail.com>
2023-09-15 19:34:58 -0400

    connect to database

---
+++ database.py
@@ -0,0 +1,15 @@
+import json
+from pymongo import MongoClient
+
+with open('config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
+url = config['mongourl']
+
+def get_database():
+
+    cluster = MongoClient(url)
+
+    db = cluster['TBD']
+
+    return db
