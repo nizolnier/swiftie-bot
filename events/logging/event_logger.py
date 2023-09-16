9f5bf4d1c6f80402e2c0f642df863d57a8e51c15
zeyf <zainy0012@gmail.com>
2023-09-16 09:52:56 -0400

    Added event logging

---
+++ events/logging/event_logger.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Text
+
+
+# PRIVATE METHODS
+def format(message: Text) -> Text:
+    current_date_time = __get_current_date_time()
+
+    return "\n".join([
+        "=====",
+        f"[EVENT LOG @ {current_date_time}]"
+        "\n",
+        f"Message: {message}",
+        "=====",
+        "\n"
+    ])
+
+def format_with_error(message: Text, error: Exception) -> Text:
+    current_date_time = __get_current_date_time()
+
+    return "\n".join([
+        "=====",
+        f"[EVENT LOG @ {current_date_time}]"
+        "\n",
+        f"Message: {message}",
+        f"Error: {error}",
+        "=====",
+        "\n"
+    ])
+
+def __get_current_date_time() -> Text:
+    current_date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    return current_date_time
+
+# PUBLIC METHODS
+def log(message: Text) -> None:
+    log_message = format(message)
+    print(log_message)
+
+def log_with_error(message: Text, error: Exception) -> None:
+    log_message_with_error = format_with_error(message, error)
+    print(log_message_with_error)
