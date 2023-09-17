430e4164a6a51d30901971c429781b829a084050
zeyf <zainy0012@gmail.com>
2023-09-16 20:59:14 -0400

    slight refactor

--- round_utils.py
+++ round_utils.py
@@ -8,6 +8,8 @@ def get_lyrics_sequence_by_difficulty(lyrics: List[Text], difficulty: Difficulty
     if len(lyrics) < lyric_lines_sequence_length:
         return "\n".join(lyrics)
     
-    random_sequence_start_index = random.randint(0, len(lyrics) - lyric_lines_sequence_length)
+    random_lyric_sequence_start_index = random.randint(0, len(lyrics) - lyric_lines_sequence_length)
 
-    return "\n".join(lyrics[random_sequence_start_index : random_sequence_start_index + lyric_lines_sequence_length:])
+    return "\n".join(
+        lyrics[random_lyric_sequence_start_index : random_lyric_sequence_start_index + lyric_lines_sequence_length:]
+    )
